07765dbcc1253aae5e619325c1075ecbbceca345
Chris Tran <christopher.v.tran@gmail.com>
2016-03-29 10:01:08 -0500

    convert date/time strings to js date/time

--- lokidb.js
+++ lokidb.js
@@ -1,113 +1,16 @@
-/************************* $.getJSON method ************************/
-/******************************************************************/
- /*function loadFileJSON(toLocalStorage, fromUrl){
-    if (localStorage[toLocalStorage])
-            { console.log("Good! Data already loaded locally! Nothing to do!");  }
-    else {
-        $.getJSON(fromUrl, function(data) { 
-            localStorage[toLocalStorage] = JSON.stringify(data);
-            console.log("Damn! Data not yet loaded locally! Ok: I am loading it!");
-        });
-      }
-    }
-// Function's firing:
-loadFileJSON( 'myData','db.json'); */
-
-
-
-/************************* $.getJSON method ************************/
-/******************************************************************/
-/*$.getJSON('db.json', function(data) {
-	document.getElementById("json").innerHTML = JSON.stringify(data, null, 4);
-})*/
-
-
-
-/************************* $.getJSON and push to array ************/
-/******************************************************************/
-/*$.getJSON('db.json', function(data) {
-	var items = [];
-	$.each(data, function(key, value) {
-		items.push("<li>key: " + key + ", " + "val: " + value.id + "</li><br>")
-	});
-
-	$("<ul/>", {
-		"class": "my-new-list",
-		html: items.join("")
-	}).appendTo("body");
-});*/
-
-
-
-/************************* for loop method ************************/
-/******************************************************************/
-/*const MAX_RECORDS = 100000;
-
-function randomString(length, chars) {
-	var result = '';
-	for (var i = length; i > 0; --i) {
-		result += chars[Math.round(Math.random() * (chars.length - 1))];
-	}
-
-	return result;
-}
-
-function createDB(items) {
-	var start = new Date().getTime();
-
-	for (var i = 1; i < (MAX_RECORDS + 1); i++) {
-		var lowtemp = Math.floor((Math.random() * 30) + 1),
-			hightemp = Math.floor((Math.random() * 200) + 1),
-			zip = Math.floor((Math.random() * 99999) + 1),
-			date = new Date().toUTCString();
-
-		items.insert(
-			{
-				id:i,
-				company_id:randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-				company_name:"company " + randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-				address: {
-					street:i + " main st",
-					city:"city" + randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-					state:randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-					zip:i + 1
-				},
-				low_temperature:lowtemp,
-				high_temperature:hightemp,
-				timestamp:date
-			}
-		);
-	}
-
-	var end = new Date().getTime();
-	var time = end - start;
-	return time;
-}
-
-// create db
-var db = new loki('testdb');
-var items = db.addCollection('testdb');
-var executionTime = createDB(items);
-console.log("<p>Execution time: " + executionTime + "</p>");
-
-// count total
-var count = items.find().length;
-console.log("<p>Total records: " + count + "</p>");
-
-// filtered records
-var filtered = items.find({'$and': [{'company_name':{'$regex': /[a-o][5-9]/i}}, {'low_temperature':{'$lt':20}}, {'high_temperature':{'$gt':120}}] });
-var str = JSON.stringify(filtered);
-var len = filtered.length;
-console.log("<p>Filtered records count: " + len + "</p>");
-//console.log("<p>" + str + "</p>");*/
-
-
-
 /************************* PapaParse csv method ************************/
 /***********************************************************************/
+
+// create db and collection
 var db = new loki('testdb');
 var coll = db.addCollection('testdb');
 
+/**
+ * parse csv into json
+ *
+ * @method     parseMe
+ * @param      {<type>}  file    { csv file }
+ */
 function parseMe(file) {
 	Papa.parse(file, {
 		header: true,
@@ -119,12 +22,33 @@ function parseMe(file) {
 	});
 }
 
+/**
+ * insert json into collection
+ *
+ * @method     renderDataset
+ * @param      {<type>}  dataset  { json data }
+ */
 function renderDataset(dataset) {
 	dataset.forEach(function(el) {
+
+
+
+		// time does not convert correctly..time zone conversion?
+		// 
+		// 
+		el.BeginUTC = moment(el.BeginUTC, "M/D/YYYY HH:mm");
+		el.EndUTC = moment(el.EndUTC, "M/D/YYYY HH:mm");
+		el.EventUTC = moment(el.EventUTC, "M/D/YYYY HH:mm");
 		coll.insert(el);
 	});
+	document.getElementById("parse-text").innerHTML = " Database populated.";
 }
 
+/**
+ * default query for total records in collection
+ *
+ * @method     defaultQuery
+ */
 function defaultQuery() {
 	// start time
 	var start = new Date().getTime();
@@ -147,6 +71,12 @@ function defaultQuery() {
 	document.getElementById("div-right").appendChild(h3node);
 }
 
+/**
+ * custom query from user input
+ *
+ * @method     customQuery
+ * @param      {string}  query   { user input query }
+ */
 function customQuery(query) {
 	var custom;
 
@@ -180,11 +110,21 @@ function customQuery(query) {
 	}
 }
 
+/**
+ * scrolls page to bottom when new results overflow right-div
+ *
+ * @method     divScroll
+ */
 function divScroll() {
 	var elem = document.getElementById("div-right");
 	elem.scrollTop = elem.scrollHeight;
 }
 
+/**
+ * calls appropriate functions on button click
+ *
+ * @method     onload
+ */
 window.onload=function() {
 	document.getElementById("parse").addEventListener('click', function() {
 		document.getElementById("parse-text").innerHTML = " Parsing...";
